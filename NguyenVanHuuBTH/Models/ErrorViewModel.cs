cd881ea49519d4b7f4e37628cbcc3fbe3ac080db
Nguyễn Văn HữuBTH <NguyenVanHuuBTH@github.com>
2023-02-20 15:51:29 +0700

    Ngay 20/02

---
+++ NguyenVanHuuBTH/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenVanHuuBTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
